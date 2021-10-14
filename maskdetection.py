2f9fab5ec3ad6ba9e72dff907e2a311b76a6411f
Vishal Gulati <vishalgulati2k@gmail.com>
2021-10-15 01:03:43 +0530

    Latest

--- maskdetection.py
+++ maskdetection.py
@@ -10,14 +10,17 @@ except Exception as e:
 oldname = sys.argv[1]
 newname=sys.argv[1]+".jpg"
 os.rename(oldname,newname)
-
 mymodel=load_model('mymodel.h5')
 test_image=image.load_img(newname,
                           target_size=(150,150,3))
 os.replace(newname,"public/image.jpg")
+
 test_image=image.img_to_array(test_image)
+
 test_image=np.expand_dims(test_image,axis=0)
+
 pred=mymodel.predict(test_image)[0][0]
+
 if pred==1:
     print("Not wearing mask")
 else:

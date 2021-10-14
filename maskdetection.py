6bc1484cf7a538787df6340de8eec0628e40f544
Vishal Gulati <vishalgulati2k@gmail.com>
2021-10-14 23:16:48 +0530

    py

--- maskdetection.py
+++ maskdetection.py
@@ -18,17 +18,19 @@ oldname = sys.argv[1]
 newname=sys.argv[1]+".jpg"
 os.rename(oldname,newname)
 
+print("file renamed")
 #newname=sys.argv[1]
 
 mymodel=load_model('mymodel.h5')
 test_image=image.load_img(newname,
                           target_size=(150,150,3))
-test_image
+
 test_image=image.img_to_array(test_image)
 test_image=np.expand_dims(test_image,axis=0)
 pred=mymodel.predict(test_image)[0][0]
+print("PRED is ",pred)
 if pred==1:
     print("Not wearing mask")
 else:
     print("Wearing mask")
-os.move(newname,"public/image.jpg")
+# os.move(newname,"public/image.jpg")

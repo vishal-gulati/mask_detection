5b2c9a211318258a97ba2db8ffe2c935dbd355a3
Vishal Gulati <vishalgulati2k@gmail.com>
2021-10-14 23:59:41 +0530

    py

--- maskdetection.py
+++ maskdetection.py
@@ -2,15 +2,8 @@ try:
     import sys
     import numpy as np
     import os
-    import keras
-    import keras.backend as k
-    from keras.layers import Conv2D,MaxPooling2D,SpatialDropout2D,Flatten,Dropout,Dense
-    from keras.models import Sequential,load_model
-    from tensorflow.keras.optimizers  import Adam
+    from keras.models import load_model
     from keras.preprocessing import image
-    import cv2
-    import datetime
-    import pandas as pd
 except Exception as e:
     print(e)
 
@@ -18,17 +11,14 @@ oldname = sys.argv[1]
 newname=sys.argv[1]+".jpg"
 os.rename(oldname,newname)
 
-#newname=sys.argv[1]
 mymodel=load_model('mymodel.h5')
 test_image=image.load_img(newname,
                           target_size=(150,150,3))
-
+os.replace(newname,"public/image.jpg")
 test_image=image.img_to_array(test_image)
 test_image=np.expand_dims(test_image,axis=0)
 pred=mymodel.predict(test_image)[0][0]
-print("PRED is ",pred)
 if pred==1:
     print("Not wearing mask")
 else:
     print("Wearing mask")
-# os.move(newname,"public/image.jpg")
